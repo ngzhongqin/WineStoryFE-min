4e6285ac8e0f5531f09c2058fb749a95e5296a20
ngzhongqin <ngzhongqin@gmail.com>
2016-01-03 16:35:45 +0800

    Order View

--- scripts/controllers/orders/view.js
+++ scripts/controllers/orders/view.js
@@ -15,5 +15,19 @@ app.controller('OrderViewCtrl', function ($scope, ngCart, $cookies, $rootScope, 
     OrderService.view_order($routeParams.param1,function(data){
         $scope.order = data.data;
     });
+    
+    $scope.to2decimal = function (raw_value){
+        return Number(raw_value).toFixed(2);   
+    }
+    
+    $scope.getShipping = function(){
+        if($scope.order != null){
+            if($scope.order.shipping_cost != 0){
+                return '$' + $scope.to2decimal($scope.order.shipping_cost);
+            }else{
+                return 'Free';   
+            }
+        }
+    }
 });
 
